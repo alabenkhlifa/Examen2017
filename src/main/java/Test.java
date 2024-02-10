b6f2ce1ee514a83f9f938bd21e09a5e0ceabfce9
Ala BEN KHLIFA <ala.benkhlifa@maibornwolff.de>
2024-02-10 17:27:36 +0100

    Add User 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
     // TODO 2
     public static void main(String[] args) {
          System.out.println("hello Nada!");
+         System.out.println("hello Nada!");
          System.out.println("welcome back!");
     }
 }
